8b89c494b31dedb9080be554c1cfffb1c671d0eb
muse254 <fanosoro@gmail.com>
2020-11-08 12:31:42 +0300

    done clippy

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -6,12 +6,13 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let x = 1.2331f64;
     let y = 1.2332f64;
-    if y != x {
+
+    // solution: PatialEq is implemented for f64,
+    // ne(&self, _other: &()) for '!='
+    if x.ne(&y){
         println!("Success!");
     }
 }
