b5ed5f378d82ee7e05ef59ec984d1b6ace103104
muse254 <fanosoro@gmail.com>
2020-11-08 11:26:34 +0300

    done variables

--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,8 +1,6 @@
 // variables6.rs
 // Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
 
-// I AM NOT DONE
-
 const NUMBER: usize  = 3; // solution: constants types must also always be annotated
 fn main() {
     println!("Number {}", NUMBER);
