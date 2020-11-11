bfc0a338e2146b4a55c9d3636942ef0a4478c301
muse254 <fanosoro@gmail.com>
2020-11-11 19:59:03 +0300

    done standard_library_types

--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,7 +1,5 @@
 // iterators4.rs
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
     // Do not use:
@@ -12,6 +10,10 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num < 1 {
+        return num;
+    }
+    (1..=num).fold(1u64, |acc, val| acc * val)
 }
 
 #[cfg(test)]
